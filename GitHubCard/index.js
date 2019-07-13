876c3934747b9f3d1d6e64b3053e8abe5fec4c1a
brettmadrid <brettmadrid9@gmail.com>
2019-07-13 01:44:21 -0700

    Initial commit

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,13 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/brettmadrid')
+.then(res => {
+  console.log(res)
+  document.querySelector('.cards').appendChild(cardsComponent(res.data))
+}).catch(err => {
+  console.log(err)
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -14,21 +21,30 @@
            create a new component and add it to the DOM as a child of .cards
 */
 
+
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
           , manually find some other users' github handles, or use the list found 
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
+          at the bottom of the page. Get at least 5 different Github useNames and add them as
           Individual strings to the friendsArray below.
           
           Using that array, iterate over it, requesting data for each user, creating a new card for each
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
+
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then(res => {
+    document.querySelector('.cards').appendChild(cardsComponent(res.data))
+  }).catch(err => {
+    console.log(err)
+  })
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
-
 <div class="card">
   <img src={image url of user} />
   <div class="card-info">
@@ -43,13 +59,49 @@ const followersArray = [];
     <p>Bio: {users bio}</p>
   </div>
 </div>
-
 */
+function cardsComponent(gitObject) {
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const url = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+  image.src = gitObject.avatar_url;
+  name.textContent = `Name: ${gitObject.name}`;
+  username.textContent = `Username: ${gitObject.login}`;
+  location.textContent = `Location: ${gitObject.location}`
+  url.textContent = gitObject.html_url;
+  url.href = gitObject.html_url;
+  followers.textContent = `Followers: ${gitObject.followers}`;
+  following.textContent = `Following: ${gitObject.following}`;
+  bio.textContent = gitObject.bio;
+
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  profile.appendChild(url)
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+  username.classList.add('username')
+
+  return card;
+
+}
+
